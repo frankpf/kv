8f35b9467c2af3e773a15beb0cfe6df8911de03f
Frank Paulo Filho <me@frankpf.com>
2017-11-03 00:22:10 -0200

    MemTable: add delete(), iter(); test for max KV sizes

--- src/lsm/mod.rs
+++ src/lsm/mod.rs
@@ -1,48 +1,85 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashSet};
+use std::collections::btree_map::Iter as BTreeIterator;
 use std::str;
 
 const MAX_MEMTABLE_SIZE: usize = 5 * 1024 * 1024;
 
+use std::{u8,u32};
+
+/* Max number of bytes the key and value may take */
+const MAX_KEY_SIZE: usize = u8::MAX as usize;
+const MAX_VAL_SIZE: usize = u32::MAX as usize;
+
+#[derive(Debug)]
 struct MemTable<'a> {
-	data: BTreeMap<&'a [u8], &'a [u8]>,
 	size: usize,
+	map: BTreeMap<&'a [u8], &'a [u8]>,
+	deleted: HashSet<&'a [u8]>,
 }
 
 impl<'a> MemTable<'a> {
 	fn new() -> MemTable<'a> {
 		MemTable {
-			data: BTreeMap::new(),
-			size: 0
+			size: 0,
+			map: BTreeMap::new(),
+			deleted: HashSet::new(),
 		}
 	}
 
 	fn get(&self, key: &[u8]) -> Option<&[u8]> {
-		match self.data.get(key) {
-			Some(v) => Some(*v),
+		if self.deleted.contains(key) {
+			return None
+		}
+
+		match self.map.get(key) {
+			Some(v) => Some(v),
 			None => None,
 		}
 	}
 
-	fn put(&mut self, key: &'a [u8], val: &'a [u8]) {
+	// TODO: Use real errors, not strings
+	fn put(&mut self, key: &'a [u8], val: &'a [u8]) -> Result<(), &'static str> {
+		if key.len() >= MAX_KEY_SIZE {
+			return Err("Key is too big")
+		}
+
+		if val.len() >= MAX_VAL_SIZE {
+			return Err("Value is too big")
+		}
+
+		if self.deleted.contains(key) {
+			self.deleted.remove(key);
+		}
+
 		self.size += key.len();
 		self.size += val.len();
 
-		self.data.insert(key, val);
+		self.map.insert(key, val);
+
+		Ok(())
 	}
 
 	fn delete(&mut self, key: &'a [u8]) -> Option<&[u8]> {
-		if let Some(val) = self.data.remove(key) {
+		if let Some(val) = self.map.remove(key) {
+			self.deleted.insert(key);
+			/* Return the deleted value */
 			self.size -= key.len();
 			self.size -= val.len();
 			Some(val)
 		} else {
+			/* Nothing was deleted */
 			None
 		}
 	}
 
 	fn clear(&mut self) {
 		self.size = 0;
-		self.data.clear();
+		self.map.clear();
+		self.deleted.clear();
+	}
+
+	fn iter(&self) -> BTreeIterator<&[u8], &[u8]> {
+		self.map.iter()
 	}
 }
 
